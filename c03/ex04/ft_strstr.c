e146390b902ceb9f29fe3a26119c01c1dab8beec
redox132 <ridaelkouri@gmail.com>
2025-07-07 20:49:49 +0200

    add c03/ex04

--- c03/ex04/ft_strstr.c
+++ c03/ex04/ft_strstr.c
@@ -1,30 +1,34 @@
 #include <stdio.h>
 
-char	*ft_strstr(char *str, char *to_find)
+char *ft_strstr(char *str, char *to_find)
 {
-	int	i;
-	int	j;
+    int i;
+    int j;
 
-	i = 0;
-	while(str[i])
-	{
-		j = 0;
-		while (str[i])
-		{
-			if (str[i] == to_find[i])
-			{
-				return &str[i];
-			}
-			j++;
-		i++;
-		}
-	}
+    if (*to_find == '\0')
+        return str;
+
+    i = 0;
+    while (str[i] != '\0')
+    {
+        j = 0;
+        while (str[i + j] != '\0' && to_find[j] != '\0' && str[i + j] == to_find[j])
+        {
+            j++;
+        }
+        if (to_find[j] == '\0')
+            return &str[i];
+        i++;
+    }
+    return 0;
 }
+
 
 int main(void)
 {
 	char str[] = "hello world!";
-	char to_find[] = "ld";
+	char to_find[] = "lo
+		;
 
 	printf("%s", ft_strstr(str, to_find));
 }
